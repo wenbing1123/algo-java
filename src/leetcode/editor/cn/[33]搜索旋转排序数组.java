1cbaf327d969dd63658a4dfd7fb775fa209f2b8e
yangwb <a12345678>
2020-01-02 11:10:14 +0800

    搜索旋转数组_两次二分查找（超时）

---
+++ src/leetcode/editor/cn/[33]搜索旋转排序数组.java
@@ -0,0 +1,88 @@
+//假设按照升序排序的数组在预先未知的某个点上进行了旋转。 
+//
+// ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。 
+//
+// 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。 
+//
+// 你可以假设数组中不存在重复的元素。 
+//
+// 你的算法时间复杂度必须是 O(log n) 级别。 
+//
+// 示例 1: 
+//
+// 输入: nums = [4,5,6,7,0,1,2], target = 0
+//输出: 4
+// 
+//
+// 示例 2: 
+//
+// 输入: nums = [4,5,6,7,0,1,2], target = 3
+//输出: -1 
+// Related Topics 数组 二分查找
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        int rotateIndex = searchRotateIndex(nums, 0, nums.length-1);
+
+        if (rotateIndex == 0) {
+            return binarySearch(nums, 0, nums.length-1, target);
+        }
+
+        if (target == nums[rotateIndex]) {
+            return rotateIndex;
+        }
+
+        if (target < nums[0]) {
+            return binarySearch(nums, rotateIndex, nums.length-1, target);
+        }
+
+        return binarySearch(nums, 0, rotateIndex, target);
+    }
+
+    private int searchRotateIndex(int[] nums, int left, int right) {
+        if (nums[left] < nums[right]) {
+            return 0;
+        }
+
+        while (left <= right) {
+            int mid = (left+right)>>1;
+
+            if (nums[mid] > nums[mid+1]) {
+                return mid + 1;
+            } else {
+                if (nums[mid] < nums[left]) {
+                    left = mid - 1;
+                } else {
+                    right = mid + 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int target){
+        while (left <= right) {
+            int mid = (left+right)>>1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

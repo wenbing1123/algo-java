ac53ff403c76a17ca7bf5c874e889c3d823e0075
yangwb <a12345678>
2020-01-07 14:17:08 +0800

    [20]最长有效括号_动态规划解法（没理解）

--- src/leetcode/editor/cn/[32]最长有效括号.java
+++ src/leetcode/editor/cn/[32]最长有效括号.java
@@ -20,38 +20,19 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestValidParentheses(String s) {
-        int res=0, left=0, right=0;
+        int res = 0;
+        int[] dp = new int[s.length()];
+        for(int i=1; i<s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if(s.charAt(i-1) == '(') {
+                    dp[i] = (i>=2 ? dp[i-2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
 
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                left++;
-            } else {
-                right++;
-            }
-
-            if (left==right) {
-                res = Math.max(res, 2*right);
-            } else if(right>=left) {
-                left=right=0;
+                res = Math.max(res, dp[i]);
             }
         }
-
-        left=right=0;
-        for(int i=s.length()-1; i>=0; i--) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                left++;
-            } else {
-                right++;
-            }
-            if (left==right) {
-                res = Math.max(res, 2*left);
-            } else if(left>=right) {
-                left=right=0;
-            }
-        }
-
         return res;
     }
 

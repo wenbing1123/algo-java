80991dc0c4be88a5e069186d8eac53563d75a2f0
yangwb <a12345678>
2020-01-07 13:41:41 +0800

    [20]最长有效括号_栈解法

--- src/leetcode/editor/cn/[32]最长有效括号.java
+++ src/leetcode/editor/cn/[32]最长有效括号.java
@@ -21,11 +21,18 @@
 class Solution {
     public int longestValidParentheses(String s) {
         int res = 0;
-
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         for(int i=0; i<s.length();i++) {
-            for(int j=i+2; j<=s.length(); j=j+2) {
-                if (isValid(s.substring(i,j))) {
-                    res = Math.max(res, j-i);
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if(stack.empty()) {
+                    stack.push(i);
+                } else {
+                    res = Math.max(res, i - stack.peek());
                 }
             }
         }
@@ -33,21 +40,5 @@ class Solution {
         return res;
     }
 
-    private boolean isValid(String s){
-        Stack<Character> stack = new Stack<>();
-
-        for (int i=0;i<s.length();i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                stack.push(c);
-            } else if (!stack.empty()) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-
-        return stack.empty();
-    }
 }
 //leetcode submit region end(Prohibit modification and deletion)

dd883c3ef5ce19435ad31fbeb2c990c96b35118e
wenbing1123@163.com <217859931>
2020-01-10 21:28:04 +0800

    [105]从前序与中序遍历序列构造二叉树_递归

--- src/leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.java
+++ src/leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.java
@@ -30,8 +30,34 @@
  * }
  */
 class Solution {
+
+    private int[] preorder;
+    private int i;
+    private HashMap<Integer,Integer> inorderMap;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
+        this.preorder = preorder;
+        this.i = 0;
+        this.inorderMap = buildMap(inorder);
+        return recur(inorder, 0, inorder.length-1);
+    }
+
+    private TreeNode recur(int[] inorder, int l, int r) {
+        if (l > r) return null;
+        int s = inorderMap.get(preorder[i]);
+        TreeNode root = new TreeNode(preorder[i]);
+        i++;
+        root.left = recur(inorder, l, s-1);
+        root.right = recur(inorder, s+1, r);
+        return root;
+    }
+
+    private HashMap<Integer, Integer> buildMap(int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return map;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)

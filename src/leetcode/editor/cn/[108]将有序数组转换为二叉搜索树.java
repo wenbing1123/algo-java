7e4c44cb64913fc538fb148aa6d7f5fc709754ab
wenbing1123@163.com <217859931>
2020-01-10 20:25:45 +0800

    [108]将有序数组转换为二叉搜索树_递归+折半查找

--- src/leetcode/editor/cn/[108]将有序数组转换为二叉搜索树.java
+++ src/leetcode/editor/cn/[108]将有序数组转换为二叉搜索树.java
@@ -30,7 +30,19 @@
  */
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-        
+        if (nums.length == 0) {
+            return null;
+        }
+        return recur(nums, 0, nums.length-1);
+    }
+
+    private TreeNode recur(int[] nums, int l, int r) {
+        if (l>r) return null;
+        int mid = (l+r)>>>1;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = recur(nums, l, mid-1);
+        node.right = recur(nums, mid+1, r);
+        return node;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)

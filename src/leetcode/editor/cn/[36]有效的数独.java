9eeee712a743ea6d8ff475ce2aa5c45fb93e7831
yangwb <a12345678>
2020-01-13 14:23:48 +0800

    [36]有效的数独_直接解法（hashmap计数+一次迭代）

--- src/leetcode/editor/cn/[36]有效的数独.java
+++ src/leetcode/editor/cn/[36]有效的数独.java
@@ -62,7 +62,35 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] cols = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];
+
+        for (int i=0; i<9; i++) {
+            rows[i] = new HashMap<>();
+            cols[i] = new HashMap<>();
+            boxes[i] = new HashMap<>();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    int n = (int) num;
+                    int k = (i / 3 ) * 3 + j / 3;
+
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+                    cols[j].put(n, cols[j].getOrDefault(n, 0) + 1);
+                    boxes[k].put(n, boxes[k].getOrDefault(n, 0) + 1);
+
+                    if (rows[i].get(n) > 1 || cols[j].get(n) > 1 || boxes[k].get(n) > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)

3c39488569b025ea4d19bf8a515270212381a789
yangwb <a12345678>
2020-01-13 11:49:15 +0800

    [200]岛屿数量_dfs

--- src/leetcode/editor/cn/[200]岛屿数量.java
+++ src/leetcode/editor/cn/[200]岛屿数量.java
@@ -27,8 +27,40 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+
     public int numIslands(char[][] grid) {
-        
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char c = grid[i][j];
+                if (c == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
     }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i<0 || i>=m || j<0 || j>=n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)

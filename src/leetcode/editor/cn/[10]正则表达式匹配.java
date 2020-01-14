9715b9de5d25162330574168a731d067552d4390
wenbing1123@163.com <217859931>
2020-01-14 22:58:19 +0800

    [10]正则表达式_回溯算法

--- src/leetcode/editor/cn/[10]正则表达式匹配.java
+++ src/leetcode/editor/cn/[10]正则表达式匹配.java
@@ -61,8 +61,16 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public boolean isMatch(String s, String p) {
 
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        boolean firstMatch = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+        if (p.length()>=2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
     }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)

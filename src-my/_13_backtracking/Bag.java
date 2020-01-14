df66a62d24c48e518406f84679adbb1a47378aff
yangwb <a12345678>
2020-01-14 11:12:46 +0800

    myAlgo_回溯算法

--- src-my/_13_backtracking/Bag.java
+++ src-my/_13_backtracking/Bag.java
@@ -1,12 +1,44 @@
 package _13_backtracking;
 
+//01-bag
 public class Bag {
 
-    public void knapsack() {
+    static final int CAPACITY = 8;
 
+    static class Tuple {
+        int weight;
+        int value;
+
+        Tuple(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
+    private int maxValue = Integer.MIN_VALUE;
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public void bag(Tuple[] items, int i, int w, int v) {
+        if (w >= CAPACITY || i == items.length) {
+            maxValue = Math.max(maxValue, v);
+            return;
+        }
+
+        bag(items, i + 1, w, v); //不放当前物品
+        if (w + items[i].weight <= CAPACITY) {
+            bag(items, i + 1, w + items[i].weight, v + items[i].value); //放当前物品
+        }
     }
 
     public static void main(String[] args) {
-        
+        var bean = new Bag();
+
+        var items = new Tuple[]{new Tuple(3, 5), new Tuple(2, 2), new Tuple(1, 4), new Tuple(1, 2), new Tuple(4, 10)};
+        bean.bag(items, 0, 0, 0);
+        System.out.println("--------maxValue---------");
+        System.out.println(bean.getMaxValue());
     }
 }
--- src-my/_13_backtracking/EightQueens.java
+++ src-my/_13_backtracking/EightQueens.java
@@ -1,8 +1,44 @@
 package _13_backtracking;
 
+import java.util.Arrays;
+
 public class EightQueens {
 
-    public static void main(String[] args) {
+    private static final int BOARD_SIZE = 8;
 
+    private int solutionCount = 0;
+    private int[] queenList = new int[BOARD_SIZE];
+
+    public void eightQueens(int row) {
+        if (row == BOARD_SIZE) {
+            solutionCount++;
+            System.out.println(Arrays.toString(queenList));
+        } else {
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                if (isValidPos(row, i)) {
+                    queenList[row] = i;
+                    eightQueens(row+1);
+                }
+            }
+        }
+    }
+
+    private boolean isValidPos(int currRow, int currCol) {
+        int i=0;
+        while (i<currRow) {
+            if (queenList[i] == currCol) return false;
+            if (currRow-i == Math.abs(currCol-queenList[i])) return false;
+            i++;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        var bean = new EightQueens();
+
+        bean.eightQueens(0);
+
+        System.out.println("\n--- solution count ---");
+        System.out.println(bean.solutionCount);
     }
 }

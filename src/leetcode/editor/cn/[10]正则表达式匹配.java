c85f3302aa5b012ac4dad985b3c6343877f05ccc
wenbing1123@163.com <217859931>
2020-01-14 23:19:20 +0800

    [10]正则表达式_动态规划

--- src/leetcode/editor/cn/[10]正则表达式匹配.java
+++ src/leetcode/editor/cn/[10]正则表达式匹配.java
@@ -64,12 +64,28 @@ class Solution {
 
     public boolean isMatch(String s, String p) {
         if (p.isEmpty()) return s.isEmpty();
-        boolean firstMatch = (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
-        if (p.length()>=2 && p.charAt(1) == '*') {
-            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
-        } else {
-            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        // dp[i][j]表示s的第i个字符及之前与p的第j个字符及之前的匹配状态
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
         }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 
 }

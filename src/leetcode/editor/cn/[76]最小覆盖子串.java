1b8a9054980cdd0864ebe080d23ca5e424f4cd61
yangwb <a12345678>
2020-01-15 14:38:40 +0800

    [176]最小覆盖子串_双指针+哈希+滑动窗口

--- src/leetcode/editor/cn/[76]最小覆盖子串.java
+++ src/leetcode/editor/cn/[76]最小覆盖子串.java
@@ -18,7 +18,43 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
 
+        HashMap<Character, Integer> dictT = new HashMap<>();
+        for (int i=0; i<t.length();i++) {
+            dictT.put(t.charAt(i), dictT.getOrDefault(t.charAt(i), 0)+1);
+        }
+
+        int l=0,r=0, formed=0,required = dictT.size();
+        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
+        int[] ans = {-1, 0, 0};
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0)+1);
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)

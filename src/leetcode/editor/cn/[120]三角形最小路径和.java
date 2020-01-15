ac78c2a4fd54fc0663f7e6b49d8ba4361fe1c62c
yangwb <a12345678>
2020-01-15 11:50:52 +0800

    [120]杨辉三角_动态规划

--- src/leetcode/editor/cn/[120]三角形最小路径和.java
+++ src/leetcode/editor/cn/[120]三角形最小路径和.java
@@ -22,7 +22,26 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        
+        int len = triangle.size();
+        int[] dp = new int[len];
+        dp[0] = triangle.get(0).get(0);
+        for (int i=1; i<len; i++) {
+            List<Integer> l = triangle.get(i);
+            for (int j=i; j>=0; j--) {
+                if (j==0) {
+                    dp[j] = dp[j] + l.get(j);
+                } else if (j==i) {
+                    dp[j] = dp[j-1] + l.get(j);;
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j-1]) + l.get(j);
+                }
+            }
+        }
+        int min = dp[0];
+        for (int i = 1; i < dp.length; i++) {
+            min = Math.min(min, dp[i]);
+        }
+        return min;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)

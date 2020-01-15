11c99e9b11ad3bb3f86a3a7af998909cab0d58cf
yangwb <a12345678>
2020-01-15 15:51:16 +0800

    [77]组合_回溯算法

--- src/leetcode/editor/cn/[77]组合.java
+++ src/leetcode/editor/cn/[77]组合.java
@@ -18,8 +18,32 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public List<List<Integer>> combine(int n, int k) {
 
+    private int n;
+    private int k;
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        if (n < k) return res;
+        this.n = n;
+        this.k = k;
+
+        LinkedList<Integer> list = new LinkedList<>();
+        backtrack(1, list);
+        return res;
+    }
+
+    private void backtrack(int curr, LinkedList<Integer> list) {
+        if (list.size() == k) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i=curr; i<=n; i++) {
+            list.add(i);
+            backtrack(i+1, list);
+            list.removeLast();
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
